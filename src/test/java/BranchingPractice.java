62682d41dbd836327bde9469108b23ce4b50efef
Gulya0626 <gulyakg@yahoo.com>
2021-05-25 18:43:16 -0500

    first commit in review1

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,12 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+
+        //created a new project
+        //enabled version control
+        //added .ignore with .idea, target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
+        //Reviewing on new branch
+    }
+}
